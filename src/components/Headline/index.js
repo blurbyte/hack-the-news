c8f0395e9299c2cc7bcc3724233c1871f02b6bf7
blurbyte <info@blurbyte.com>
2017-02-23 23:44:13 +0100

    Headline component created

---
+++ src/components/Headline/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const StyledComment = styled.h1`
+  color: #fff;
+  font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-size: 4.2rem;
+  font-weight: 400;
+  line-height: 1.2;
+  letter-spacing: -0.1rem;
+  margin: 0;
+`;
+
+export default StyledComment;
