82d43f0ba7604cc78f9c454c63a2ab904f097309
blurbyte <info@blurbyte.com>
2017-02-24 23:19:51 +0100

    Simple loader created

---
+++ src/components/Loader/Loading.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Loading = styled.p`
+  display: block;
+  background-color: #fe6e0d;
+  border-radius: 1rem;
+  padding: 1rem 1.6rem;
+  color: #fff;
+`;
+
+export default Loading;
---
+++ src/components/Loader/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Loading from './Loading';
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 4.2rem;
+`;
+
+const Loader = () => {
+  return (
+    <Wrapper>
+      <Loading>Loading...</Loading>
+    </Wrapper>
+  );
+};
+
+export default Loader;
