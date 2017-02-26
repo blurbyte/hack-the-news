65b826aba9b5bccd6200a61f0e90f901c1129956
blurbyte <info@blurbyte.com>
2017-02-26 22:25:04 +0100

    Message look updated

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -2,9 +2,11 @@ import styled from 'styled-components';
 
 const Message = styled.p`
   color: #aaa;
+  background-color: #fffcfb;
   font-style: oblique;
-  margin-top: 4.2rem;
-  margin-left: 4.2rem;
+  padding: 4.2rem;
+  margin: 0;
+  border-top: 4px solid #f4f0ed;
 `;
 
 export default Message;
